2f21d484f29b0705a4191285091e2af2fcf78da9
MARIANNA MONARI CORDEIRO <marianna.cordeiro@portalsesisp.org.br>
2026-05-06 10:58:16 -0300

    feat: adicionando funcao de detalhes de alunos
    adicionando nova funcao

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import PHome from './pages/PHome/PHome'
-import PLogin from './components/PLogin/PLogin'
+import PLogin from './pages/PLogin/PLogin'
 import PListagemAluno from './pages/PListagem/PLIstagemAluno/PListagemAluno'
-import PListagemLivro from './pages/PListagem/PListagemLivro/PListagemLivro'
 import PListagemEmprestimo from './pages/PListagem/PListagemEmprestimo/PListagemEmprestimo'
-
+import PListagemLivro from './pages/PListagem/PListagemLivro/PListagemLivro'
+import PDetalhesAluno from './pages/PDetalhes/PDetalhesAlunos/PDetalhesAluno'
+// import ProtectedRoute from './components/Rotas/ProtectedRoutes'
 
 function App() {
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<PHome />} />
         <Route path='/login' element={<PLogin />} />
-        <Route path='/lista/aluno' element={<PListagemAluno />} />
-        <Route path='/lista/livro' element={<PListagemLivro />} />
-        <Route path='/lista/emprestimo' element={<PListagemEmprestimo />} />
+        <Route path='/lista/alunos' element={<PListagemAluno />} />
+        <Route path='/lista/emprestimos' element={<PListagemEmprestimo />} />
+        <Route path='/lista/livros' element={<PListagemLivro />} />
+        <Route path='/detalhes/aluno/:id_aluno' element={<PDetalhesAluno />} />
       </Routes>
     </BrowserRouter>
   )
